c3a84346e214e0bf8bc946a675bef5a6a4e560ac
Rishabh <geekrishabh@gmail.com>
2020-05-25 16:52:19 +0530

    navigation change

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
           <Route path="/about">
             <About />
           </Route>
           <Route path="/users">
             <Users />
+          </Route>
+          <Route path="/">
+            <Home />
           </Route>
         </Switch>
       </Router>
